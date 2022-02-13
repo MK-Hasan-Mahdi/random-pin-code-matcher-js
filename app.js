5f09901169813dabc74891bcd7728effcb284674
mk-hasan <mkhasan6891@gmail.com>
2022-02-14 01:20:07 +0600

    Secret random code mathcer first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+function getPin() {
+    const pin = Math.round(Math.random() * 10000);
+    const pinString = pin + '';
+    if (pinString.length == 4) {
+        return pin;
+    }
+    else {
+        return getPin();
+    }
+}
+
+function generatePin() {
+
+
+}
+
+
+
+const displayPin = document.getElementById('display-pin');
+displayPin.value = generatePin();
